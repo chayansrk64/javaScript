25721d47fa23e659c79fc3b2f472260203417280
chayansrk64 <chayansrk64@gmail.com>
2025-08-05 23:46:51 +0600

    array.from() method added

--- array/iteration.js
+++ array/iteration.js
@@ -26,3 +26,45 @@
      }
     //  console.log(result)
 }
+// JavaScript Array reduce()
+{
+    const numbers = [45, 4, 9, 16, 25];
+    let sum = numbers.reduce(reducer);
+    function reducer(total, value, index, array){
+        return total + value;
+    }
+    // console.log(sum)
+}
+// JavaScript Array every()
+// The every() method checks if all array values pass a test;
+{
+    const numbers = [45, 4, 9, 16, 25];
+    let result = numbers.every(checker);
+    function checker(value, index, array){
+        return value % 2 === 0; // checks every element is even number;
+    }
+    // console.log(result) // false;
+
+}
+// JavaScript Array some()
+// The some() method checks if some array vlaues pass a test.
+{
+    const numbers = [45, 4, 9, 16, 25];
+    let result = numbers.some(checker);
+    function checker(value, index, array){
+        return value % 2 === 0; // checks some element is even number;
+    }
+    // console.log(result) // true;
+}
+
+// JavaScript Array.from() ======
+/**
+ * The Array.from() method returns an Array object from:
+ * 1. Any iterable object;
+ * 2. Any object with a length property;
+ */
+{
+    let text = 'abcdefg';
+    let result = Array.from(text);
+    console.log(result)
+}
