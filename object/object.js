054249770c9ff871092ddeafe6653a2fdcb69d72
chayansrk64 <chayansrk64@gmail.com>
2025-08-08 11:12:47 +0600

    object initialised

---
+++ object/object.js
@@ -0,0 +1,30 @@
+
+/**
+ *  JavaScript Object Definition
+ *  How to define a Object?
+ *  # using an Object Literal;
+ *  # using the new Keyword;
+ *  # using an Object Constructor; 
+ * 
+ *  */
+
+// Using an Object Literal
+// an object literal is a list of named valued inside curly braces { }
+// In object terms, the named values are called key:value pairs.
+// these key:value pairs are called object properties.
+
+{
+    const myObject = {
+        firstName:"John",
+        lastName:"Doe",
+        age:50,
+        eyeColor:"blue"
+    }
+}
+{
+    const person = {};
+    person.name = "Manik";
+    person.age = 34;
+    person.profession = "Teacher"
+    console.log(person)
+}
