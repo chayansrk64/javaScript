14fb37e840c33c6cf5dbd7e2c6e3def44d338aa0
chayansrk64 <chayansrk64@gmail.com>
2025-08-10 17:03:52 +0600

    prototype added

---
+++ object/prototype.js
@@ -0,0 +1,12 @@
+/**
+ * Prototype Inheritance:
+ * All JavaScript objects inherit properties and methods form a prototype.
+ * # Date objects inherit from Date.prototype;
+ * # Array objects inherit form Array.prototype;
+ * # Person objects inherit from Person.prototype;
+ * 
+ * The Object.prototype is on the top of the prototype inheritance chain:
+ * 
+ * Date objects, Array objects, and Person objects inherit from Object.prototype.
+ * 
+ *
